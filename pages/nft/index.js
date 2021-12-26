c017584d6e34d9cbc84db670c30b804ae4ad9ea8
Alex <alex@wealthparadigm.org>
2021-12-26 21:02:20 +0100

    added staticProps to nft

--- pages/nft/index.js
+++ pages/nft/index.js
@@ -10,5 +10,19 @@ export default function Nfts({ nfts }) {
     )
 }
 
+const options = {
+    method: 'GET',
+    headers: { Accept: 'application/json', 'X-API-KEY': process.env.NEXT_PUBLIC_OPENSEA_KEY }
+};
 
+export async function getStaticProps() {
+    const res = await fetch('https://api.opensea.io/wyvern/v1/orders?bundled=false&include_bundled=false&limit=20&offset=10&order_by=created_date&order_direction=desc', options);
 
+    const nfts = await res.json()
+
+    return {
+        props: {
+            nfts,
+        },
+    }
+}
