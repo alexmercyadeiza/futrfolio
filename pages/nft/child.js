f84797ef9a9a870e12bb8d2a760a3d7c55e1bcca
Alex <alex@wealthparadigm.org>
2021-12-26 21:59:01 +0100

    nfts link to opensea

--- pages/nft/child.js
+++ pages/nft/child.js
@@ -1,12 +1,15 @@
 import Image from "next/image"
 
-export default function Child({ name, currency_image, currency, amount, nft_image }) {
+export default function Child({ name, currency_image, currency, amount, nft_image, opensea_url }) {
 
     const defaultImage = 'https://images.pexels.com/photos/5702943/pexels-photo-5702943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
 
     return (
         <div>
-            <img loading="lazy" className="object-center rounded-xl object-cover mb-5 w-full md:w-60 h-60" src={nft_image ? nft_image : defaultImage} />
+            <a target="_blank" href={opensea_url}>
+                <img loading="lazy" className="object-center rounded-xl object-cover mb-5 w-full md:w-60 h-60" src={nft_image ? nft_image : defaultImage} />
+            </a>
+
             <div>
                 <div className="font-bold">{name}</div>
                 <div className="text-xs space-x-1">
@@ -14,6 +17,7 @@ export default function Child({ name, currency_image, currency, amount, nft_imag
                     <span className="font-bold">
                         {`${(amount / 1000000000000000000).toFixed(2)}`}
                     </span>
+                    <span>{currency}</span>
                 </div>
             </div>
         </div>
--- pages/nft/index.js
+++ pages/nft/index.js
@@ -4,7 +4,7 @@ export default function Nfts({ nfts }) {
     return (
         <div className="grid md:grid-cols-5 gap-8">
             {nfts.orders.map(nft =>
-                <Child key={nft.asset.id} nft_image={nft.asset.image_url} name={nft.asset.asset_contract.name} currency={nft.payment_token_contract.symbol} currency_image={nft.payment_token_contract.image_url} amount={nft.base_price} />
+                <Child key={nft.asset.id} opensea_url={nft.asset.permalink} nft_image={nft.asset.image_url} name={nft.asset.asset_contract.name} currency={nft.payment_token_contract.symbol} currency_image={nft.payment_token_contract.image_url} amount={nft.base_price} />
             )}
         </div>
     )
